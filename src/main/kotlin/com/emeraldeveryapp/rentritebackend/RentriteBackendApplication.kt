523f027d87018deec3244f8c10eac9a732a34e2f
Maeve <maevereilly21@gmail.com>
2024-05-11 17:07:39 -0700

    Initial commit for spring backend
    Add skeleton files for spring app.

---
+++ src/main/kotlin/com/emeraldeveryapp/rentritebackend/RentriteBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.emeraldeveryapp.rentritebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RentriteBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<RentriteBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/emeraldeveryapp/rentritebackend/RentriteBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.emeraldeveryapp.rentritebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RentriteBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
